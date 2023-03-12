6b02166abf825a5e66ed8eceb0852f68dc757f45
Alhassan <raad.albadri@ug.bilkent.edu.tr>
2023-03-12 17:54:50 +0300

    test: Intentionally fail test checker
    This commit adds a new test to the test suite that intentionally fails
    the test checker. The purpose of this test is to ensure that the test
    checker is working correctly and can detect failures. Will be removed
    in the next commit.

--- src/test/java/me/kreaktech/unility/post/PostRepositoryTest.java
+++ src/test/java/me/kreaktech/unility/post/PostRepositoryTest.java
@@ -22,6 +22,12 @@ public class PostRepositoryTest {
 	@Autowired
 	private PostRepository postRepository;
 
+	// This test will deliberately fail
+	@Test
+	public void PostRepository_WillFail() {
+		Assertions.assertThat(1).isEqualTo(2);
+	}
+
 	@Test
 	public void PostRepository_SaveAll_ReturnSavedPost() {
 		// Arrange
