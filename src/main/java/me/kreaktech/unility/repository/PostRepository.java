b34fb51c6b161cae1095fa70cce14332ec5f9a17
Alhassan <raad.albadri@ug.bilkent.edu.tr>
2023-03-12 14:49:54 +0300

    refactor: Change findByDateBetween in PostRepository to include the end date
    This change fixes the issue where the end date was not included in the
    query.

--- src/main/java/me/kreaktech/unility/repository/PostRepository.java
+++ src/main/java/me/kreaktech/unility/repository/PostRepository.java
@@ -1,14 +1,21 @@
 package me.kreaktech.unility.repository;
 
-import java.util.Date;
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import me.kreaktech.unility.entity.Post;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
-	List<Post> findByDateBetween(Date from, Date to);
+	default List<Post> findByDateBetween(Timestamp from, Timestamp to) {
+		return this.findByDateBetweenAndDateLessThanEqual(from, to);
+	}
+	
+	
+    @Query("SELECT p FROM Post p WHERE p.date BETWEEN ?1 AND ?2 AND p.date <= ?2")
+	List<Post> findByDateBetweenAndDateLessThanEqual(Timestamp from, Timestamp to);
 	Optional<Post> findByTitle(String title);
 }
