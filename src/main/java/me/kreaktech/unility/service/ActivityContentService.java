247c0c0a88b83952f23ca77560fa42af9c1d36b3
Egehan <egehanyldz7@gmail.com>
2023-04-18 18:09:33 +0300

    refactor + feat: Some changes in ActivityController made and ActivityContent layers added.
    -Also a method called findByTitle in ActivityContentRepository is added because its a possibility that a customer may want to recieve the content with its title

---
+++ src/main/java/me/kreaktech/unility/service/ActivityContentService.java
@@ -0,0 +1,10 @@
+package me.kreaktech.unility.service;
+
+import me.kreaktech.unility.entity.ActivityContent;
+
+public interface ActivityContentService {
+    ActivityContent getActivityContentById(Integer id);
+    ActivityContent saveActivityContent(ActivityContent activityContent);
+    void deleteActivityContentById(Integer id);
+    ActivityContent getByTitle(String title);
+}
