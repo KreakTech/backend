34ab3bb560337f0dab48fe72b9821b1d4a479551
Egehan <egehanyldz7@gmail.com>
2023-04-18 17:11:27 +0300

    feat: Activity related repository, service, serviceImpl and controller classes added.

---
+++ src/main/java/me/kreaktech/unility/repository/ActivityRepository.java
@@ -0,0 +1,9 @@
+package me.kreaktech.unility.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import me.kreaktech.unility.entity.Activity;
+
+public interface ActivityRepository extends JpaRepository<Activity, Integer>{
+    
+}
---
+++ src/main/java/me/kreaktech/unility/service/ActivityServiceImpl.java
@@ -0,0 +1,41 @@
+package me.kreaktech.unility.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import me.kreaktech.unility.entity.Activity;
+import me.kreaktech.unility.repository.ActivityRepository;
+import me.kreaktech.unility.utils.Utils;
+
+@AllArgsConstructor
+@Service
+public class ActivityServiceImpl implements ActivityService{
+
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    @Override
+    public Activity getActivityById(Integer id) {
+        Optional<Activity> activity = activityRepository.findById(id);
+        return Utils.unwrap(activity,id);
+    }
+
+    @Override
+    public Activity saveActivity(Activity activity) {
+        return activityRepository.save(activity);
+    }
+
+    @Override
+    public void deleteActivityById(Integer id) {
+        activityRepository.deleteById(id);
+    }    
+
+    @Override
+    public List<Activity> getAllActivities(){
+        return activityRepository.findAll();
+    }
+}
