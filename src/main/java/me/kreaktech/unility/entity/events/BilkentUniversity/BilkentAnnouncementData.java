a9910e69a919ebdbc6c950ddd8c3522bb8a6b4df
Alhassan <raad.albadri@ug.bilkent.edu.tr>
2023-05-19 12:11:31 +0300

    chore: Remove unused  imports

--- src/main/java/me/kreaktech/unility/entity/events/BilkentUniversity/BilkentAnnouncementData.java
+++ src/main/java/me/kreaktech/unility/entity/events/BilkentUniversity/BilkentAnnouncementData.java
@@ -1,10 +1,8 @@
 package me.kreaktech.unility.entity.events.BilkentUniversity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import me.kreaktech.unility.constants.Enum.Language;
 import me.kreaktech.unility.entity.events.DefaultUniversity.DefaultAnnouncementData;
 
 @Getter
